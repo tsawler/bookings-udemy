124179d50e82676158f5bf1489863c0f728a0c1d
Trevor Sawler <trevor.sawler@gmail.com>
2021-01-16 14:14:35 -0400

    Error checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -15,7 +16,27 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d"), sum)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// Divide divides one value into another and returns message with result
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
 }
 
 // addValues adds two ints x and y, and returns the sum
@@ -27,6 +48,7 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
