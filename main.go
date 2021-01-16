214e7305b2156e41d7d0c9fc822e81ede2cec198
Trevor Sawler <trevor.sawler@gmail.com>
2021-01-16 14:02:21 -0400

    Functions and handlers

--- main.go
+++ main.go
@@ -7,19 +7,23 @@ import (
 
 const portNumber = ":8080"
 
+// Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page")
 }
 
+// About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := addValues(2, 2)
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d"), sum)
 }
 
+// addValues adds two ints x and y, and returns the sum
 func addValues(x, y int) int {
 	return x + y
 }
 
+// main is the main function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
