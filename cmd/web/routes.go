50fe22150d944d9efd4e94c6696d59b08d98eac9
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-09 19:49:07 -0400

    use chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,15 @@ package main
 import (
 	"bookings-udemy/pkg/config"
 	"bookings-udemy/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module bookings-udemy
 
 go 1.15
 
-require github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
+require github.com/go-chi/chi v1.5.1
