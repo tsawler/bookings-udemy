45a6bcac4e32cf5717243f7deeae33887fc2e1ea
Trevor Sawler <trevor.sawler@gmail.com>
2021-02-09 19:51:22 -0400

    use chi for routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,11 +4,14 @@ import (
 	"bookings-udemy/pkg/config"
 	"bookings-udemy/pkg/handlers"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
